8eb8069e8b21d78985eb1d4cfea10cd266df39d3
Pranitha Rallabandi <40241348+pranithacse@users.noreply.github.com>
2021-02-16 09:47:45 +0530

    converjsonbuffer.js

---
+++ convertjsonbuffer.js
@@ -0,0 +1,12 @@
+ 
+const msg = '{"name":"Pranithar", "age":"22"}';
+ 
+var jsonObj = JSON.parse(msg);
+ 
+// convert JSON object to String
+var jsonStr = JSON.stringify(jsonObj);
+ 
+// read json string to Buffer
+const buf = Buffer.from(jsonStr);
+ 
+console.log(buf.length);
